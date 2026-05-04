763eb4772614dc7d827ce81f23da9ac89f561430
ConDiaz <connor.diaz@my.utsa.edu>
2026-05-03 22:48:09 -0500

    Remove Folder

--- Unified Gaming Platform/Game.java
+++
@@ -1,7 +0,0 @@
-public class Game {
-    String gameID, title, genre, requiredVersion;
-    Game(String id, String title, String genre, String version) {
-        this.gameID = id; this.title = title;
-        this.genre = genre; this.requiredVersion = version;
-    }
-}
